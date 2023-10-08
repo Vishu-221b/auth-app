70e48b7ab93762a2e057a92c5c809adc46d7dd87
Vishu-221b <vishwajeetsinghchauhan63@gmail.com>
2023-10-08 10:36:11 +0530

    email-verification done

--- src/app/api/users/signup/route.ts
+++ src/app/api/users/signup/route.ts
@@ -2,9 +2,11 @@ import {connect} from "@/dbConfig/dbConfig";
 import User from "@/models/userModel";
 import { NextRequest, NextResponse } from "next/server";
 import bcryptjs from "bcryptjs";
+import { sendEmail } from "@/helpers/mailer";
 
 
-connect();
+connect()
+
 
 export async function POST(request: NextRequest){
     try {
@@ -33,14 +35,20 @@ export async function POST(request: NextRequest){
         const savedUser = await newUser.save()
         console.log(savedUser);
 
+        //send verification email
+
+        await sendEmail({email, emailType: "VERIFY", userId: savedUser._id})
+
         return NextResponse.json({
             message: "User created successfully",
             success: true,
             savedUser
         })
-    }
-    
-    catch (error: any) {
+        
+        
+
+
+    } catch (error: any) {
         return NextResponse.json({error: error.message}, {status: 500})
 
     }
